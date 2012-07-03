cab6ea8a2e4a25f40228b20f1816da45e01581c8
rodumani <devnull@localhost>
2012-07-04 00:47:26 +0900

    디자인 변경

--- src/com/sparcs/nugum/PersonActivity.java
+++ src/com/sparcs/nugum/PersonActivity.java
@@ -1,10 +1,18 @@
 package com.sparcs.nugum;
 
+import java.util.ArrayList;
+
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.provider.ContactsContract;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
+import android.content.ContentProviderOperation;
+import android.content.ContentProviderResult;
 import android.content.Intent;
+import android.content.OperationApplicationException;
+import android.graphics.Color;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,6 +20,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.support.v4.app.NavUtils;
 
 public class PersonActivity extends Activity {
@@ -22,20 +31,27 @@ public class PersonActivity extends Activity {
         setContentView(R.layout.activity_person);
         
         TextView nameText= (TextView)findViewById(R.id.PersonActivityTextName);
-        TextView pagerText= (TextView)findViewById(R.id.PersonActivityTextPager);
-        TextView sparcsmailText=(TextView)findViewById(R.id.PersonActivityTextSPARCSMail);
+        //TextView pagerText= (TextView)findViewById(R.id.PersonActivityTextPager);
+        //TextView sparcsmailText=(TextView)findViewById(R.id.PersonActivityTextSPARCSMail);
         Button callButton = (Button)findViewById(R.id.PersonActivityButtonCall);
         Button textButton = (Button)findViewById(R.id.PersonActivityButtonText);
         Button sparcsmailButton = (Button)findViewById(R.id.PresonActivitySendSPARCSMail);
+        Button addContactButton = (Button)findViewById(R.id.PersonActivityAddContact);
         
         intent=getIntent();
         nameText.setText(intent.getStringExtra("name"));
-        pagerText.setText(intent.getStringExtra("pager"));
-        sparcsmailText.setText(intent.getStringExtra("sparcsID")+"@sparcs.org");
+        //pagerText.setText(intent.getStringExtra("pager"));
+        //sparcsmailText.setText(intent.getStringExtra("sparcsID")+"@sparcs.org");
+        callButton.setText("전화번호 : "+intent.getStringExtra("pager"));
+        callButton.setBackgroundColor(Color.rgb(240, 240, 240));
+        sparcsmailButton.setText("이메일 : "+intent.getStringExtra("sparcsID")+"@sparcs.org");
+        sparcsmailButton.setBackgroundColor(Color.rgb(240, 240, 240));
+        
         if(intent.getStringExtra("pager").equals(""))
         {
-        	pagerText.setText("전화번호가 없습니다.");
-        	callButton.setVisibility(4);
+        	//pagerText.setText("전화번호가 없습니다.");
+        	callButton.setText("전화번호 : 전화번호가 없습니다.");
+        	callButton.setEnabled(false);
         	textButton.setVisibility(4);
         }
         
@@ -69,9 +85,17 @@ public class PersonActivity extends Activity {
 				Intent it = new Intent(Intent.ACTION_SENDTO, uri);
 				startActivity(it);
 			}
+		});    
+        
+        addContactButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Toast.makeText(PersonActivity.this,"아직 준비중인 기능입니다.",Toast.LENGTH_SHORT).show();
+			}
 		});
-        
-        
+
     }
 
     @Override
