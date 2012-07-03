57e9e350cee7be85e2b00e10ced722de871e17ff
rodumani <devnull@localhost>
2012-07-04 03:45:50 +0900

    인증도입

--- gen/com/sparcs/nugum/R.java
+++ gen/com/sparcs/nugum/R.java
@@ -20,31 +20,40 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int EditText01=0x7f080000;
-        public static final int ListView01=0x7f080001;
-        public static final int PersonActivityAddContact=0x7f080007;
-        public static final int PersonActivityButtonCall=0x7f080003;
-        public static final int PersonActivityButtonText=0x7f080006;
-        public static final int PersonActivitySendMail=0x7f080005;
-        public static final int PersonActivityTextName=0x7f080002;
-        public static final int PresonActivitySendSPARCSMail=0x7f080004;
-        public static final int menu_settings=0x7f080008;
+        public static final int EditText01=0x7f080003;
+        public static final int ListView01=0x7f080004;
+        public static final int LoginActivityBT=0x7f080002;
+        public static final int LoginActivityIDEdit=0x7f080000;
+        public static final int LoginActivityPWEdit=0x7f080001;
+        public static final int PersonActivityAddContact=0x7f08000a;
+        public static final int PersonActivityButtonCall=0x7f080006;
+        public static final int PersonActivityButtonText=0x7f080009;
+        public static final int PersonActivitySendMail=0x7f080008;
+        public static final int PersonActivityTextName=0x7f080005;
+        public static final int PresonActivitySendSPARCSMail=0x7f080007;
+        public static final int menu_settings=0x7f08000b;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_person=0x7f030001;
+        public static final int activity_login=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_person=0x7f030002;
     }
     public static final class menu {
-        public static final int activity_main=0x7f070000;
-        public static final int activity_person=0x7f070001;
+        public static final int activity_login=0x7f070000;
+        public static final int activity_main=0x7f070001;
+        public static final int activity_person=0x7f070002;
     }
     public static final class string {
+        public static final int ID=0x7f050009;
+        public static final int Password=0x7f05000a;
         public static final int addContact=0x7f050007;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int login=0x7f05000b;
         public static final int menu_settings=0x7f050002;
         public static final int name=0x7f050004;
         public static final int sendText=0x7f050006;
+        public static final int title_activity_login=0x7f050008;
         public static final int title_activity_main=0x7f050003;
         public static final int title_activity_person=0x7f050005;
     }
